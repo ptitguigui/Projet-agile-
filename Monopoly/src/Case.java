e59b56b8f5a945c0242d4874517a0cdfc7311186
BARRY <BARRY@192.168.1.62>
2016-08-31 22:54:18 +0200

    creation des classes Case, Joueur et Terrain

---
+++ Monopoly/src/Case.java
@@ -0,0 +1,16 @@
+
+public class Case {
+	 protected String nom;
+     protected int numero;
+
+     public Case(String nom, int num) {
+		this.nom=nom;
+		this.numero=num;
+	}
+
+	public String getNom()
+     {
+        return nom;
+     }
+
+}
---
+++ Monopoly/src/Terrain.java
@@ -0,0 +1,37 @@
+
+public class Terrain extends Case{
+		protected int prix;
+	    protected int loyer;
+	    protected Joueur proprio;
+	    public Terrain(String nom,int num,int p, int l, Joueur j){
+	        super(nom,num);
+	        prix=p;
+	        loyer=l;
+	        proprio=j;
+	    }
+
+	    public int getPrix(){
+	        return (prix);
+	    }
+
+	    public Joueur getProprietaire(){
+	        return (proprio);
+	    }
+
+	    public void setProprietaire(Joueur j){
+	        proprio=j;
+	    }
+
+	    public boolean aVendre(){
+	        return (proprio==null);
+	    }
+
+	    public boolean aLouer(){
+	        return(proprio!=null);
+	    }
+
+	    public int calculerLoyer(){
+	        return (loyer);
+	    }
+
+}
