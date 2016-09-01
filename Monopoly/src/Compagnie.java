ad14629b27665d307f15aaaeab981c0daedb3f5f
Aminatou Barry <aminatou.barry97@gmail.com>
2016-09-01 10:02:21 +0200

    conflit

--- Monopoly/src/Compagnie.java
+++ Monopoly/src/Compagnie.java
@@ -1,12 +1,14 @@
 
 
 public class Compagnie extends Terrain{
-
 	public Compagnie(String nom, int num, int p, int l, Joueur j) {
-		super(nom, num, p, l, j);
-		
+		super(nom, num, p, l, j);		
 	}
 	public int calculloyer (){
+		int de=(int)(12*Math.random());
+        int pay;
+        //if (nomGroupe.uniqueProprietaire(proprio) )
+        {pay=de*1000;
 		return 0;
 	}
 	
