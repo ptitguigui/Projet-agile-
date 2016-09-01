bb92b823bc173d5b8aa857cb0309bb35e729d9bf
NeroKyusi <vasseur.samson@gmail.com>
2016-09-01 09:31:38 +0200

    modifié :         Monopoly/bin/.gitignore 	modifié :         Monopoly/src/Test.java

--- Monopoly/src/Test.java
+++ Monopoly/src/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");	
 		System.out.println("Test Vianney");
-		System.out.println("Test Samson");
+		System.out.println("Test Samson2");
 	}	
 }
