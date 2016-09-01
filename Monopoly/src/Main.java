a77b7ef87c561404f7e33f48917d0df2d6f814d3
Guillaume.Lepretre <http.proxy>
2016-09-01 09:31:58 +0200

    conflits

---
+++ Monopoly/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		do{
+		Scanner in = new Scanner(System.in);
+		int nombreJoueurs = in.nextInt();
+		
+		}while (!(nombreJoueurs.matches("[1-4]*"));
+		
+	}
+}
--- Monopoly/src/test/DésTest.java
+++ Monopoly/src/test/DésTest.java
@@ -1,9 +1,23 @@
 package test;
+im
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
+public class DésTest(){
 
 
+@Test
+public void dé(){
+Dés dés = new Dés();
+dés.roll();
+}
 
+/*
+@Test
+public void note_without_info_should_give_NONE() {
+	Note note = new Note();
+	assertEquals(note.getType(), "NONE" );
+}
+*/
+}
