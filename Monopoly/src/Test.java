73b10ba0a54dc07a996cd8ef290dcf64460d3fcd
NeroKyusi <vasseur.samson@gmail.com>
2016-09-01 09:24:44 +0200

    Test Samson

--- Monopoly/src/Test.java
+++ Monopoly/src/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");	
 		System.out.println("Test Vianney");
+		System.out.println("Test Samson");
 	}	
 }
