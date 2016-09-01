ddc5cdc99e6f2e22632d7e694874d494e12bed6f
NeroKyusi <vasseur.samson@gmail.com>
2016-09-01 11:16:43 +0200

    MaJ TpT

--- Monopoly/src/Main.java
+++ Monopoly/src/Main.java
@@ -1,23 +1,33 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 import gameplay.Joueur;
+import gameplay.ListeJoueurs;
 
 public class Main {
 	public static void main(String[] args) {
-		ArrayList<Joueur> joueurs = new ArrayList<>();
+		ListeJoueurs listeJoueurs = new ListeJoueurs();
 		System.out.println("Bienvenue dans Monopoly !\nCombien de joueur y a-t-il ?");
 		Scanner in = new Scanner(System.in);
 		int nbJR = in.nextInt();
 		String name;
+		
 		for(int i = 0; i<nbJR; i++){
 			name = JOptionPane.showInputDialog(null, "Entrez votre pseudo");
-			joueurs.add(new Joueur(name));
+			listeJoueurs.ajouterJoueur(new Joueur(name));
 		}
 		while(true){
-			
+			System.out.println("C'est à "+ listeJoueurs.getJoueurActuel() + " de jouer" );
+			System.out.println("Que voulez-vous faire?");
+			System.out.println("4. Passez votre tour");
+			Scanner in2= new Scanner(System.in);
+			int choix = in2.nextInt();
+			if (choix==4) {
+				listeJoueurs.passerTour(); 
+			} else {
+				System.out.println("Vous ne pouvez pas faire ça");
+			}
 		}
 	}
 }
--- Monopoly/src/gameplay/ListeJoueurs.java
+++ Monopoly/src/gameplay/ListeJoueurs.java
@@ -2,69 +2,12 @@ package gameplay;
 
 import java.util.ArrayList;
 
-public class Jouer {
+public class ListeJoueurs {
 	
 	private ArrayList<Joueur> listeJoueurs = new ArrayList<Joueur>();
 	private int JoueurActuel; 
 	
-	public Jouer (Joueur j) {
-		listeJoueurs.add(j);
-	}
-	
-	public Jouer (Joueur j, Joueur j2) {
-		listeJoueurs.add(j);
-		listeJoueurs.add(j2);
-	}
-	
-	public Jouer (Joueur j, Joueur j2, Joueur j3) {
-		listeJoueurs.add(j);
-		listeJoueurs.add(j2);
-		listeJoueurs.add(j3);
-	}
-	
-	public Jouer (Joueur j, Joueur j2, Joueur j3, Joueur j4) {
-		listeJoueurs.add(j);
-		listeJoueurs.add(j2);
-		listeJoueurs.add(j3);
-		listeJoueurs.add(j4);
-	}
-	
-	public Jouer (Joueur j, Joueur j2, Joueur j3, Joueur j4, Joueur j5) {
-		listeJoueurs.add(j);
-		listeJoueurs.add(j2);
-		listeJoueurs.add(j3);
-		listeJoueurs.add(j4);
-		listeJoueurs.add(j5);
-	}
-	
-	public Jouer (Joueur j, Joueur j2, Joueur j3, Joueur j4, Joueur j5, Joueur j6) {
-		listeJoueurs.add(j);
-		listeJoueurs.add(j2);
-		listeJoueurs.add(j3);
-		listeJoueurs.add(j4);
-		listeJoueurs.add(j5);
-		listeJoueurs.add(j6);
-	}
-	
-	public Jouer (Joueur j, Joueur j2, Joueur j3, Joueur j4, Joueur j5, Joueur j6, Joueur j7) {
-		listeJoueurs.add(j);
-		listeJoueurs.add(j2);
-		listeJoueurs.add(j3);
-		listeJoueurs.add(j4);
-		listeJoueurs.add(j5);
-		listeJoueurs.add(j6);
-		listeJoueurs.add(j7);
-	}
-	
-	public Jouer (Joueur j, Joueur j2, Joueur j3, Joueur j4, Joueur j5, Joueur j6, Joueur j7, Joueur j8) {
-		listeJoueurs.add(j);
-		listeJoueurs.add(j2);
-		listeJoueurs.add(j3);
-		listeJoueurs.add(j4);
-		listeJoueurs.add(j5);
-		listeJoueurs.add(j6);
-		listeJoueurs.add(j7);
-		listeJoueurs.add(j8);
+	public ListeJoueurs() {
 	}
 	
 	
@@ -81,5 +24,9 @@ public class Jouer {
 		if (this.JoueurActuel>(listeJoueurs.size()-1)) {
 			this.JoueurActuel=0;
 		}
+	}
+	
+	public void ajouterJoueur(Joueur j) {
+		listeJoueurs.add(j);
 	}
 }
