aae9f1b5c2585a842c1e194d4b42a50e4c5857b6
Kévin Régnier <kevin.regnier@etudiant.univ-lille.fr>
2016-09-02 08:22:10 +0200

    ajouts cartes chance et communauté

--- Monopoly/src/monopoly/plateau/Plateau.java
+++ Monopoly/src/monopoly/plateau/Plateau.java
@@ -20,7 +20,36 @@ public class Plateau {
 		this.plateau.add(new Terrain("Chance", 7, 0, 0, 0, 0, 0, 0, 0, 0, null));
 		this.plateau.add(new Terrain("Rue de Courcelles", 8, 100, 0, 0, 0, 0, 0, 0, 0, null));
 		this.plateau.add(new Terrain("Avenue de la République", 9, 0, 0, 0, 0, 0, 0, 0, 0, null));
-		this.plateau.add(new Terrain("Prison",  10, 0, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Prison / Simple visite",  10, 0, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Boulevard de la Villette",  11, 140, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Compagnie d'Electricite",  12, 150, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Avenue de Neuilly",  13, 140, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Rue du Paradis",  14, 160, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Gare("Gare de Lyon",  15, 200, 0,null,null));
+		this.plateau.add(new Terrain("Avenue Mozart",  16, 180, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Caisse de Communaute",  17, 0, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Boulevard Saint Michel",  18, 180, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Place Pigalle",  19, 200, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Parc Gratuit",  20, 0, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Avenue Matignon",  21, 220, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Chance",22, 220, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Boulevard Malesherbes",23, 220, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Avenue Henri Martin",  24, 240, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Gare("Gare du Nord",25, 200, 0,null,null));
+		this.plateau.add(new Terrain("Faubourg Saint Honore",26, 260, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Place de la Bourse",27, 260, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Compagnie des Eaux",28, 150, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Rue la Fayette",29, 280, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Allez en prison",30, 0, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Avenue de Breteuil",31, 300, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Avenue Foch",32, 300, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Caisse de Communaute",33,0, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Boulevard des Capucines",34,320, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Gare("Gare Saint Lazare",35, 200, 0,null,null));
+		this.plateau.add(new Terrain("Chance",36,0, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Avenue des Champs-Elysees",37,350, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Taxe de Luxe",38, 100, 0, 0, 0, 0, 0, 0, 0, null));
+		this.plateau.add(new Terrain("Rue de la Paix",39,400, 0, 0, 0, 0, 0, 0, 0, null));
 		
 	}
 	public void parcourirPlateau(){
